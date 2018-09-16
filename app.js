295de68e266da9bb11b4017f2874e06198576564
Juan <juanl0215@gmail.com>
2018-09-16 01:01:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ function getJokes(e) {
   xhr.open('GET', `https://api.icndb.com/jokes/random/${number}`, true);
 
   xhr.onload = function() {
+    let output = '';
     if (this.status === 200) {
       const response = JSON.parse(this.responseText);
-      console.log(response);
-      let jokes = '';
-      response.value.forEach(function(joke) {
-        jokes += `
-        <li>Joke: ${joke.joke}</li>`;
-      });
-      document.querySelector('.jokes').innerHTML = jokes;
+      let output = '';
+      if (response.type === 'success') {
+        response.value.forEach(function(joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += `<li>Something went wrong</li>`;
+      }
+      document.querySelector('.jokes').innerHTML = output;
     }
   };
 
